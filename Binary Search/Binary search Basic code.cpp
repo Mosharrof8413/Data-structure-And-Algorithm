c0bc15da5f3c94b67d86312e4bbeeb31b374420f
Mosharrof8413 <50889365+Mosharrof8413@users.noreply.github.com>
2021-06-14 01:24:50 +0600

    Add files via upload

---
+++ Binary Search/Binary search Basic code.cpp
@@ -0,0 +1,68 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define pb push_back
+#define ll long long
+#define mk make_pair
+#define endl '\n'
+#define M  1000000007
+ull dp[101][101];
+ull nCr(ull n,ull r)
+{
+    if(n==r)return dp[n][r]=1;
+    if(r==1)return dp[n][r]=n;
+    if(r==0)return dp[n][r]=1;
+    if(dp[n][r])return dp[n][r];
+    return dp[n][r]=nCr(n-1,r-1)+nCr(n-1,r);
+}
+
+ll mod(ll x)
+{
+    return ((x%M+M)%M);    // x will be negative or positive
+}
+ll add(ll a,ll b)
+{
+    return mod((mod(a)+mod(b)));
+}
+ll mul(ll a,ll b)
+{
+    return mod(mod(a)*mod(b));
+}
+bool cmp(const pair<ll,ll>&p1, pair<ll,ll>&p2)
+{
+    if(p1.first>p2.first)return 1;
+    else if(p1.first==p2.first)return (p1.second<p2.second);
+    else return 0;
+}
+bool is_balanced(char first,char second )
+{
+    return ((first=='('&& second==')')||(first=='{' && second=='}') || (first=='[' && second==']'));
+}
+
+int a[]={1,3,5,6,7,8,9,10};
+// current size of array n=8
+//Checking the target value in the array.if target value is present then return the index of target value .else return -1...
+ ll binarySearch(ll target)
+{
+    ll n=8; // array size
+    ll l=0,r=n-1;
+    while(l<=r)
+    {
+        ll mid=(l+r)/2;
+        if(a[mid]==target)return mid;
+        else if(a[mid]<target)
+            l=mid+1;
+        else r=mid-1;
+    }
+     return -1;
+
+
+}
+int main()
+{
+   ll target;
+   cin>>target;
+   cout<<binarySearch(target)<<endl;
+}
+//Alhamdulillah...
