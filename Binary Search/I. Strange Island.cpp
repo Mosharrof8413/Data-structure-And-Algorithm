e49671bf0df3cc0a919b6d605d5bc35c7a0d7645
Mosharrof8413 <50889365+Mosharrof8413@users.noreply.github.com>
2021-06-18 03:29:43 +0600

    Add files via upload

---
+++ Binary Search/I. Strange Island.cpp
@@ -0,0 +1,110 @@
+/*
+  Problem link : https://algo.codemarshal.org/contests/ncpc18/problems/I
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+//
+//debug
+template<typename F,typename S>ostream&operator<<(ostream&os,const pair<F,S>&p){return os<<"("<<p.first<<", "<<p.second<<")";}
+template<typename T>ostream&operator<<(ostream&os,const vector<T>&v){os<<"{";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"}";}
+template<typename T>ostream&operator<<(ostream&os,const set<T>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<",";os<<*it;}return os<<"]";}
+template<typename T>ostream&operator<<(ostream&os,const multiset<T>&v) {os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"]";}
+template<typename F,typename S>ostream&operator<<(ostream&os,const map<F,S>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<it->first<<" = "<<it->second;}return os<<"]";}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu(){cerr << endl;}
+template<typename T>void faltu(T a[],int n){for(int i=0;i<n;++i)cerr<<a[i]<<' ';cerr<<endl;}
+template<typename T,typename...hello>void faltu(T arg,const hello&...rest){cerr<<arg<<' ';faltu(rest...);}
+//#else
+//#define dbg(args...)
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+
+
+const int mx=1e5+123;
+ll a[mx];
+ll check(ll d,ll m,ll k)
+{
+    ll last=a[1]+d;
+    k--;
+   for(ll i=2;i<=m;i++)
+   {
+      if(abs(a[i]-last)<=d)continue;
+      else
+      {
+            if(k==0)
+          {
+             return 0;
+          }
+          last=a[i]+d;
+          k--;
+      }
+   }
+   return 1;
+}
+int main()
+{
+    optimize();
+    ll test;
+    cin>>test;
+    for(ll tc=1;tc<=test;tc++)
+    {
+       ll n,m,k;
+       cin>>n>>m>>k;
+       for(ll i=1;i<=m;i++)cin>>a[i];
+       ll l=0,r=n;
+       ll ans;
+       while(l<=r)
+       {
+           ll mid=(l+r)>>1;
+           if(check(mid,m,k)==1)
+           {
+               ans=mid;
+               r=mid-1;
+           }
+           else l=mid+1;
+       }
+       cout<<"Case "<<tc<<": "<<ans<<endl;
+
+    }
+
+}
+//Alhamdulillah...
+
+
